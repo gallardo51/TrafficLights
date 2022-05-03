eb7d9475c70bae4ec676899df6afe0f4689b709b
Alex Sobolev <31108@mail.ru>
2022-05-03 21:12:04 +0300

    Setup code for traffic light

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -20,10 +20,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startButtonLabel.layer.cornerRadius = 10
         
+        redViewLabel.layer.cornerRadius = 60
+        yellowViewLabel.layer.cornerRadius = 60
+        greenViewLabel.layer.cornerRadius = 60
+        
+        redViewLabel.alpha = 0.3
+        yellowViewLabel.alpha = 0.3
+        greenViewLabel.alpha = 0.3
+        
+        
     }
 
     @IBAction func startButtonPressed() {
+        
+        if startButtonLabel.currentTitle == "START" {
+            startButtonLabel.setTitle("NEXT", for: .normal)
+        }
+        if isLightOn(redViewLabel) {
+            redViewLabel.alpha = 0.3
+            yellowViewLabel.alpha = 1
+        } else if isLightOn(yellowViewLabel) {
+            yellowViewLabel.alpha = 0.3
+            greenViewLabel.alpha = 1
+        } else {
+            greenViewLabel.alpha = 0.3
+            redViewLabel.alpha = 1
+        }
     }
-    
+    func isLightOn (_ color: UIView) -> Bool {
+        color.alpha == 1 ? true : false
+    }
 }
-
