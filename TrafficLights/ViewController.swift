421778151c69f97c0cd5a8dcb59fc8ae26185a07
Alex Sobolev <31108@mail.ru>
2022-05-03 17:23:17 +0300

    Setup START button and setup outlets in code

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var trafficLightsLabel: UIStackView!
+    
+    @IBOutlet weak var redTrafficLightsLabel: UIView!
+    
+    @IBOutlet weak var yellowTrafficLightsLabel: UIView!
+    
+    @IBOutlet weak var greenTrafficLightsLabel: UIView!
+    
+    @IBOutlet weak var startButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
